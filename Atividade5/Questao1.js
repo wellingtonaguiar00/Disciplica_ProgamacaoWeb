26afa643ac710fff96fa738cc7f7cb647a04b105
DevWellingtonAguiar <145286622+wellingtonaguiar00@users.noreply.github.com>
2024-03-26 22:59:04 +0000

    primeiro commit

---
+++ Atividade5/Questao1.js
@@ -0,0 +1,12 @@
+function exibirMensagemBoasVindas() {
+    var nomeUsuario = prompt("Qual é o seu nome?");
+
+    if (nomeUsuario) {
+        alert("Olá, " + nomeUsuario + "! Bem-vindo à nossa página.");
+    } else {
+        alert("Bem-vindo à nossa página!");
+    }
+}
+
+
+window.onload = exibirMensagemBoasVindas;
